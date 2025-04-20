1a887c90ffca6ad52cb74de01deb757115a4b704
lpsgabriel <gabriel.lpst7@gmail.com>
2025-04-20 15:02:41 -0300

    style: center pages content

--- src/layouts/SignedInLayout.tsx
+++ src/layouts/SignedInLayout.tsx
@@ -5,11 +5,13 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <div className="relative">
+      <div className="relative flex-1 flex flex-col">
         <div className="absolute -left-2.5 top-1 z-20">
           <SidebarTrigger />
         </div>
-        <main>{children}</main>
+        <main className="flex-1 flex flex-col items-center pt-8">
+          {children}
+        </main>
       </div>
     </SidebarProvider>
   );
